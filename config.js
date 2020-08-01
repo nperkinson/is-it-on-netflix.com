17582bc703e7efb73fcf2565dc648db5f9fe9108
Nick Perkinson <nickperkinson@nprMBP.local>
2020-08-01 17:54:40 -0400

    Revise subtitle

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteTitle: 'Gatsby Starter Eventually', // <title>
-  manifestName: 'Eventually',
+  manifestName: 'Is it on Netflix?',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
@@ -9,29 +9,24 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Eventually',
+  heading: 'Wheretowatch.com',
   subHeading:
-    "A simple template for telling the world when you'll launch your next big thing.",
+    "Netflix, Hulu, Amazon Prime, Disney+ and HBO. Check the top 5 most popular streaming platforms for TV shows and movies.",
   socialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/nperkinson',
     },
     {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
-    {
-      icon: 'fa-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/nick-perkinson/',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:n.perkinson@icloud.com',
     },
   ],
 };
