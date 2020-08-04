e3d2c550d15d2809a9e8244b961517a6fc5bec43
Nick Perkinson <nickperkinson@nprMBP.local>
2020-08-03 23:17:29 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-35c736c8bbcf572757f9.js"
+    "url": "webpack-runtime-82c57140f6a1efe6590f.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c3916cfd009a51deb4a34e14338ff1d1"
+    "revision": "14af64b7f90fe37dee8bdcbf0dd0b8f5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
